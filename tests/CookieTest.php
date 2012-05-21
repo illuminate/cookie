a15eb2ea8924aeb3ff7dc7cf980e61220384fa81
Taylor Otwell <taylorotwell@gmail.com>
2012-05-21 16:42:04 -0500

    added more tests.

--- tests/CookieTest.php
+++ tests/CookieTest.php
@@ -9,6 +9,10 @@ class CookieTest extends PHPUnit_Framework_TestCase {
 		$cookie = new Cookie('/path', '/domain', true, false);
 		$c = $cookie->make('color', 'blue', 10);
 		$this->assertEquals('blue', $c->getValue());
+		$this->assertFalse($c->isHttpOnly());
+		$this->assertTrue($c->isSecure());
+		$this->assertEquals('/domain', $c->getDomain());
+		$this->assertEquals('/path', $c->getPath());
 	}
 
 }
