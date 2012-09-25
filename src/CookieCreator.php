1a20dd161bc5761635af7593c5ff05acac9e4149
Taylor Otwell <taylorotwell@gmail.com>
2012-09-25 19:26:53 -0400

    Add fingerprinting to cookie class.

--- src/CookieCreator.php
+++ src/CookieCreator.php
@@ -1,15 +1,25 @@
 <?php namespace Illuminate;
 
+use Closure;
 use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CookieCreator {
 
-	/**
-	 * The cookies that have been created.
+	/*
+	 * The current request instance.
 	 *
-	 * @var array
+	 * @var Symfony\Component\HttpFoundation\Request
 	 */
-	protected $cookies = array();
+	protected $request;
+
+	/**
+	 * The secret key used for fingerprinting.
+	 *
+	 * @var string
+	 */
+	protected $key;
 
 	/**
 	 * The default cookie options.
@@ -19,17 +29,70 @@ class CookieCreator {
 	protected $defaults = array();
 
 	/**
+	 * The cookies queued by the creator.
+	 *
+	 * @var array
+	 */
+	protected $queued = array();
+
+	/**
 	 * Create a new cookie manager instance.
 	 *
-	 * @param  string  $path
-	 * @param  string  $domain
-	 * @param  bool    $secure
-	 * @param  bool    $httpOnly
+	 * @param  Symfony\Component\HttpFoundation\Request  $request
+	 * @param  string  $key
+	 * @param  array   $defaults
 	 * @return void
 	 */
-	public function __construct($path = '/', $domain = null, $secure = false, $httpOnly = true)
+	public function __construct(Request $request, $key, array $defaults)
 	{
-		$this->defaults = compact('path', 'domain', 'secure', 'httpOnly');
+		$this->key = $key;
+		$this->request = $request;
+		$this->defaults = $defaults;
+	}
+
+	/**
+	 * Determine if a cookie exists and is not null.
+	 *
+	 * @param  string  $key
+	 * @return bool
+	 */
+	public function has($key)
+	{
+		return ! is_null($this->get($key));
+	}
+
+	/**
+	 * Get the value of the given cookie.
+	 *
+	 * @param  string  $key
+	 * @param  mixed   $default
+	 * @return mixed
+	 */
+	public function get($key, $default = null)
+	{
+		$value = $this->parse($this->request->cookies->get($key));
+
+		if (is_null($value))
+		{
+			return $default instanceof Closure ? $default() : $default;
+		}
+
+		return $value;
+	}
+
+	/**
+	 * Put a new cookie in the queue.
+	 *
+	 * @param  string  $name
+	 * @param  string  $value
+	 * @param  int     $minutes
+	 * @return Symfony\Component\HttpFoundation\Cookie
+	 */
+	public function put($name, $value, $minutes = 0)
+	{
+		$this->queued[] = $cookie = $this->make($name, $value, $minutes);
+
+		return $cookie;
 	}
 
 	/**
@@ -53,7 +116,7 @@ class CookieCreator {
 			$time = time() + ($minutes * 60);
 		}
 
-		return $this->cookies[] = new Cookie($name, $value, $time, $path, $domain, $secure, $httpOnly);
+		return new Cookie($name, $this->hash($value).'+'.$value, $time, $path, $domain, $secure, $httpOnly);
 	}
 
 	/**
@@ -80,13 +143,69 @@ class CookieCreator {
 	}
 
 	/**
-	 * Get all of the cookies that have been created.
+	 * Hash the given cookie value using the key.
 	 *
-	 * @return array
+	 * @param  string  $value
+	 * @return string
 	 */
-	public function getCookies()
+	public function hash($value)
 	{
-		return $this->cookies;
+		return hash_hmac('sha1', $value, $this->key);
+	}
+
+	/**
+	 * Parse the fingerprinted cookie value.
+	 *
+	 * @param  string       $value
+	 * @return string|null
+	 */
+	protected function parse($value)
+	{
+		$segments = explode('+', $value);
+
+		// If there are not even two segments in the array, it means that the cookie
+		// value wasn't set by the application or was changed on the client so we
+		// will return "null" since we can't assume this cookie values is safe.
+		if (count($segments) < 2)
+		{
+			return null;
+		}
+
+		$value = implode('+', array_slice($segments, 1));
+
+		// If the first segment in the arrays and the hash of the remaining segments
+		// are not equal, it means the cookie has been changed on this client and
+		// we will return null since we cannot be assured this cookie is valid.
+		if ($this->hash($value) != $segments[0])
+		{
+			return null;
+		}
+
+		return $value;
+	}
+
+	/**
+	 * Set all of the queued cookies on a Response instance.
+	 *
+	 * @param  Symfony\Component\HttpFoundation\Response  $response
+	 * @return void
+	 */
+	public function moveQueued(Response $response)
+	{
+		foreach ($this->queued as $cookie)
+		{
+			$response->headers->setCookie($cookie);
+		}
+	}
+
+	/**
+	 * Get the request instance.
+	 *
+	 * @return Symfony\Component\HttpFoundation\Request
+	 */
+	public function getRequest()
+	{
+		return $this->request;
 	}
 
 }
--- tests/CookieTest.php
+++ tests/CookieTest.php
@@ -1,25 +1,60 @@
 <?php
 
+use Mockery as m;
 use Illuminate\CookieCreator;
+use Symfony\Component\HttpFoundation\Request;
 
 class CookieTest extends PHPUnit_Framework_TestCase {
 
+	public function tearDown()
+	{
+		m::close();
+	}
+
+
 	public function testCookiesAreCreatedWithProperOptions()
 	{
-		$cookie = new CookieCreator('/path', '/domain', true, false);
+		$cookie = $this->getCreator();
 		$c = $cookie->make('color', 'blue', 10);
-		$this->assertEquals('blue', $c->getValue());
+		$this->assertEquals($cookie->hash('blue').'+blue', $c->getValue());
 		$this->assertFalse($c->isHttpOnly());
 		$this->assertTrue($c->isSecure());
 		$this->assertEquals('/domain', $c->getDomain());
 		$this->assertEquals('/path', $c->getPath());
 
 		$c2 = $cookie->forever('color', 'blue');
-		$this->assertEquals('blue', $c->getValue());
+		$this->assertEquals($cookie->hash('blue').'+blue', $c->getValue());
 		$this->assertFalse($c->isHttpOnly());
 		$this->assertTrue($c->isSecure());
 		$this->assertEquals('/domain', $c->getDomain());
 		$this->assertEquals('/path', $c->getPath());
+	}
+
+
+	public function testCookiesAreProperlyParsed()
+	{
+		$cookie = $this->getCreator();
+		$cookie->getRequest()->cookies->set('foo', $cookie->hash('bar').'+bar');
+		$this->assertEquals('bar', $cookie->get('foo'));
+
+		$cookie = $this->getCreator();
+		$cookie->getRequest()->cookies->set('foo', $cookie->hash('bar').'bar');
+		$this->assertNull($cookie->get('foo'));
+
+		$cookie = $this->getCreator();
+		$cookie->getRequest()->cookies->set('foo', $cookie->hash('bar').'291+bar');
+		$this->assertNull($cookie->get('foo'));
+	}
+
+
+	public function getCreator()
+	{
+		return new CookieCreator(Request::create('/foo', 'GET'), 'foo-bar', array(
+			'path'     => '/path',
+			'domain'   => '/domain',
+			'secure'   => true,
+			'httpOnly' => false,
+		));
 	}
 
 }
